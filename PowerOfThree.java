194c52d93f28e1acaa71dd5c40d7448114c21c42
lavanyabasavaiah <154490753+lavanyabasavaiah@users.noreply.github.com>
2024-07-12 22:06:20 +0530

    Create PowerOfThree.java

---
+++ PowerOfThree.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(isPowerOfThree(27));
+    }
+
+    static boolean isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
+    }
+}
