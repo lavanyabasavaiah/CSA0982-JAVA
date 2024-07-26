591038bdd193de9b70766ec549611968106c7f52
lavanyabasavaiah <154490753+lavanyabasavaiah@users.noreply.github.com>
2024-07-26 20:39:44 +0530

    Create Box.java

---
+++ Box.java
@@ -0,0 +1,8 @@
+class Box {
+    double w = 10, h = 10, d = 10;
+    double volume() { return w * h * d; }
+
+    public static void main(String[] args) {
+        System.out.println("Volume: " + new Box().volume());
+    }
+}
