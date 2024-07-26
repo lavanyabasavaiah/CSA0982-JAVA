78fb06db7997b63000ab25c8555b31170c0e00a1
lavanyabasavaiah <154490753+lavanyabasavaiah@users.noreply.github.com>
2024-07-26 12:27:51 +0530

    Create SumOfNumbers.java

---
+++ SumOfNumbers.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SumOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        for (int i = 0; i < n; i++) numbers[i] = scanner.nextInt();
+        int sum = 0;
+        try {
+            for (int i = 0; i <= n; i++) sum += numbers[i];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(sum);
+        }
+    }
+}
+20
